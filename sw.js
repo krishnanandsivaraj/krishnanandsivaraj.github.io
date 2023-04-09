9486e078ff5c061d53146d4ed21ea98ee793de13
krishna <ext_ksivaraj@falabella.cl>
2023-04-09 23:32:06 +0530

    update blog

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210509.1516';
+const cacheName = 'chirpy-20230409.2034';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
